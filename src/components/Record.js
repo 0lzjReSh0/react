2df603ee549fc9ec01de22138efd00a859cd9958
naNa7mi <106884285+0lzjReSh0@users.noreply.github.com>
2023-01-16 18:33:59 +0200

    1.16

--- src/components/Record.js
+++ src/components/Record.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import * as RecordsAPI from '../api/RecordsAPI';
-import { TableCell, TableRow } from "grommet"
-import { Button, } from 'grommet';
-import { Trash, Edit, Update } from 'grommet-icons';
-import { Cancel } from '@mui/icons-material';
+import PropTypes from 'prop-types';// Importing PropTypes for typechecking the props passed to the component
+import * as RecordsAPI from '../api/RecordsAPI';// Importing the API file for handling the HTTP requests
+import { TableCell, TableRow } from "grommet"// Importing the grommet TableCell and TableRow components
+import { Button, } from 'grommet';// Importing the grommet Button component
+import { Trash, Edit, Update } from 'grommet-icons';// Importing the grommet icons for delete and edit buttons
+import { Cancel } from '@mui/icons-material';// Importing the Cancel from @mui/icons-material
+// Record component
 export default class Record extends Component {
   constructor() {
     super();
@@ -20,26 +21,32 @@ export default class Record extends Component {
       }
     );
   }
-  //delete
+  // Function to handle the deletion of a record
   DeleteRecords(event) {
     event.preventDefault();
+    // Calling the remove method of the RecordsAPI with the record id
     RecordsAPI.remove(this.props.record.id).then(
       response => this.props.handleDeleteRecord(this.props.record)
+      //passing the deleted record to the parent component's handleDeleteRecord method
     )
   }
+  // Function to handle the editing of a record
   handleEdit(event) {
     event.preventDefault();
     const record = {
-      date: this.refs.date.value,
-      title: this.refs.title.value,
-      amount: Number.parseInt(this.refs.amount.value, 0)
+      date: this.refs.date.value,  // Getting the value of the date input field
+      title: this.refs.title.value, // Getting the value of the title input field
+      amount: Number.parseInt(this.refs.amount.value, 0)// Getting the value of the amount input field and parsing it to an integer
     }
+    // Calling the update method of the RecordsAPI with the record id and the updated record data
     RecordsAPI.update(this.props.record.id, record).then(
       response => {
+        // On success, passing the updated record and response data to the parent component's handleEditRecord method
         this.props.handleEditRecord(this.props.record, response.data);
       }
     )
   }
+  // Function to render the record in read-only mode
   recordRow() {
     return (
       <TableRow>
@@ -56,7 +63,7 @@ export default class Record extends Component {
       </TableRow>
     );
   }
-
+  //Build the corresponding form
   recordForm() {
     return (
       <TableRow>
